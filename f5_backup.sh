bcc3951a6acedc99e9f3d02c1f243672ff94ca2b
Sonali Jain <Sonali.Jain@ihsmarkit.com>
2024-01-30 19:27:18 +0530

    removed bash script

--- f5_backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#pip3 install GitPython
-npm install axios 
-
-# Clone Git repository
-#git clone -c http.sslVerify=false https://sonali.jain:Nzg1Njc1ODkxMjk1OtOUttUTTM1SoRZXazPL9egsQvx3@stash.mgmt.local/scm/merc/sample-project-test.git
-
-# Execute Python script
-python3 scripts/commit.py
