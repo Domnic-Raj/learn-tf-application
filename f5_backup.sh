49ea48875e50d097195476ae186e7988b17c4c2c
Sonali Jain <Sonali.Jain@ihsmarkit.com>
2024-01-30 13:36:57 +0000

    f5_backup.sh edited online with Bitbucket

--- f5_backup.sh
+++ f5_backup.sh
@@ -3,7 +3,7 @@
 npm install axios 
 
 # Clone Git repository
-git clone -c http.sslVerify=false https://sonali.jain:Nzg1Njc1ODkxMjk1OtOUttUTTM1SoRZXazPL9egsQvx3@stash.mgmt.local/scm/merc/sample-project-test.git
+#git clone -c http.sslVerify=false https://sonali.jain:Nzg1Njc1ODkxMjk1OtOUttUTTM1SoRZXazPL9egsQvx3@stash.mgmt.local/scm/merc/sample-project-test.git
 
 # Execute Python script
 python3 scripts/commit.py
