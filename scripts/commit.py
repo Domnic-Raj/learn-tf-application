8781d29b22df7bfb5792c5e3a27a7cb8a9d93385
Sonali Jain <Sonali.Jain@ihsmarkit.com>
2024-03-12 13:21:38 +0000

    commit.py edited online with Bitbucket

--- scripts/commit.py
+++ scripts/commit.py
@@ -6,7 +6,9 @@ import json
 import pathlib
 import requests
 from git import GitCommandError
-
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 F5_NON_PROD = 'https://mod-ptc-nonprod.mdgapp.net'
 F5_PROD_PTC = 'https://mod-ptc-prod.mdgapp.net'
@@ -61,91 +63,82 @@ def run():
 
     virtualservers_data = fetch_data(F5_PROD_CTC + '/mgmt/tm/ltm/virtual/?expandSubcollections=true&%24select=name,enabled,partition,pool,profilesReference/items/name,rules')
     backup_data(virtualservers_data, 'virtualservers/prod-ctc')
-     
-def get_last_commit_date(repo):
-    if not repo.heads:
-        print("Repository is empty. No commits found.")
-        return None
-    last_commit = repo.head.commit
-    return last_commit.committed_datetime
-
-  
-def pull_changes_from_origin():
-    try:
-        # Run 'git pull' command
-        origin_url = 'https://sonali.jain:Nzg1Njc1ODkxMjk1OtOUttUTTM1SoRZXazPL9egsQvx3@stash.mgmt.local/scm/merc/f5_backup.git'
-        result = subprocess.run(['git', 'pull', '-v', '--', origin_url], capture_output=True, text=True, check=True)
-        
-        # Print the output of the command
-        print(result.stdout)
-        
-        # Return True if successful
-        return True
-    
-    except subprocess.CalledProcessError as e:
-        # If the 'git pull' command fails, print the error message and return False
-        print(f"Error pulling changes from origin: {e.stderr}")
-        return False
-      
-      
-def check_for_changes(repo):
-    try:
-        # Fetch changes from the remote repository
-        repo.remotes.origin.fetch()
-        if not repo.remotes.origin.refs:
-            print("Remote repository is empty. Skipping pull operation.")
-            return True
-        # Pull changes from the remote repository
-        if not pull_changes_from_origin():
-            return False
-
-        # Check if there are any changes in the local repository
-        if repo.is_dirty(untracked_files=True) or repo.untracked_files:
-            return True
-        else:
-            return False
-    except GitCommandError as e:
-        print(f"Error pulling changes from origin: {e}")
-        return False
-    except AttributeError as e:
-        print(f"Error checking for changes: {e}")
-        return True
-def update_repository(repo):
-    repo.remotes.origin.pull()
-    repo.remotes.origin.update()
-    print("Repository updated.")
-
-def save_metadata(repo, last_commit_date):
-    with open("metadata.txt", "w") as file:
-        file.write(f"Last commit date: {last_commit_date}\n")
-        file.write("Update timestamp: {}\n".format(datetime.datetime.now()))
-
-def set_remote_origin(url):
-    try:
-        subprocess.run(['git', 'remote', 'set-url', 'origin', url], check=True)
-        print("Remote origin URL set successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error setting remote origin URL: {e}")
-def push_changes(repo):
-    repo.remotes.origin.push()
-    print("Changes pushed to remote repository.")
-
-def add_and_commit_changes(repo):
-    repo.git.add(all=True)
-    repo.git.commit('-m', 'Automated commit - Changes detected')
 
 def git_clone(repository_url, target_directory):
     try:
-        os.environ['GIT_SSL_NO_VERIFY'] = 'true'
         repo = Repo.clone_from(repository_url, target_directory)
         print(f"Repository cloned successfully to {target_directory}")
+        return repo
     except Exception as e:
         print(f"Error cloning repository: {e}")
+        return None
 
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+def git_pull(repo):
+    try:
+        repo.remotes.origin.pull()
+        print("Changes pulled from remote repository.")
+        return True
+    except Exception as e:
+        print(f"Error pulling changes from remote repository: {e}")
+        return False
 
+def git_add_and_commit(repo, commit_message):
+    try:
+        repo.git.add(all=True)
+        repo.index.commit(commit_message)
+        print("Changes staged and committed locally.")
+        return True
+    except Exception as e:
+        print(f"Error staging and committing changes locally: {e}")
+        return False
+
+def git_push(repo):
+    try:
+        repo.remotes.origin.push()
+        print("Changes pushed to remote repository.")
+        return True
+    except Exception as e:
+        print(f"Error pushing changes to remote repository: {e}")
+        return False
+
+def check_for_changes(repo):
+    try:
+        if repo.is_dirty(untracked_files=True) or repo.untracked_files:
+            print("Local changes detected.")
+            return True
+        else:
+            print("No local changes detected.")
+            return False
+    except Exception as e:
+        print(f"Error checking for local changes: {e}")
+        return False
+
+def get_last_commit_date(repo):
+    try:
+        last_commit = repo.head.commit
+        last_commit_date = last_commit.committed_datetime
+        print(f"Last commit date: {last_commit_date}")
+        return last_commit_date
+    except Exception as e:
+        print(f"Error retrieving last commit date: {e}")
+        return None
+
+def get_tracked_files_from_repo(repo_path):
+    try:
+        # Change directory to the cloned repository
+        os.chdir(repo_path)
+
+        # Run the git ls-files command to list tracked files
+        git_ls_files_output = subprocess.check_output(['git', 'ls-files'], universal_newlines=True)
+        
+        # Split the output into lines
+        tracked_files = git_ls_files_output.strip().split('\n')
+        
+        return tracked_files
+    
+    except subprocess.CalledProcessError:
+        print("Error: Git command failed")
+        return None
 def send_email(smtp_server, sender_email, recipient_email, subject, content, body_as_html=False, attachment=None):
     try:
         # Set up the email message
@@ -166,85 +159,57 @@ def send_email(smtp_server, sender_email, recipient_email, subject, content, bod
     except Exception as e:
         print(f"Failed to send email: {e}")
 
-def get_tracked_files_from_repo(repo_path):
-    try:
-        # Change directory to the cloned repository
-        os.chdir(repo_path)
-
-        # Run the git ls-files command to list tracked files
-        git_ls_files_output = subprocess.check_output(['git', 'ls-files'], universal_newlines=True)
-        
-        # Split the output into lines
-        tracked_files = git_ls_files_output.strip().split('\n')
-        
-        return tracked_files
-    
-    except subprocess.CalledProcessError:
-        print("Error: Git command failed")
-        return None
-
-
 def main():
+    # Replace these variables with the actual repository URL and target directory
     repository_url = 'https://sonali.jain:Nzg1Njc1ODkxMjk1OtOUttUTTM1SoRZXazPL9egsQvx3@stash.mgmt.local/scm/merc/f5_backup.git'
-    
-
-    # Replace 'path/to/your/target/directory' with the path where you want to clone the repository
     target_directory = 'f5_backup'
 
-    git_clone(repository_url, target_directory)
-    # Replace 'path/to/your/repo' with the actual path to your Git repository
-    repo_path = 'f5_backup/'
-
+    # Clone the Git repository
+    repo = git_clone(repository_url, target_directory)
+    if repo is None:
+        return
     # Open the Git repository
-    repo = Repo(repo_path)
-    origin = repo.create_remote('origin', url=repository_url)
-
-    # Get the date of the last commit
+    repo = Repo(target_directory)
+    run()
     last_commit_date = get_last_commit_date(repo)
-    if last_commit_date is not None:
-        print(f"Last commit date: {last_commit_date}")
-    else:
+    if last_commit_date is None:
         print("No last commit date available due to empty repository.")
-
-# Use subprocess to execute the Node.js script
-    try:
-        run()
-    except subprocess.CalledProcessError as e:
-        print(f"Error executing Node.js script: {e}")
-
-    # Check for changes
+    # Check for local changes
     if check_for_changes(repo):
-        print("Changes detected. Updating repository...")
-        # status = git_status(repo_path)
-
-        # Add and commit changes
-        add_and_commit_changes(repo)
-
-        # Save metadata
-        save_metadata(repo, last_commit_date)
-        set_remote_origin(repository_url)
-        # new_repo_path = "../f5_backup"
-        tracked_files = get_tracked_files_from_repo(repo_path)
-        push_changes(repo)
-        # Example usage
-        smtp_server = 'os-smtpp702.prod.mdgapp.net'
-        sender_email = 'sonali.jain@ihsmarkit.com'
-        recipient_email = ['sonali.jain@ihsmarkit.com','sachin.kumar4@ihsmarkit.com']
-        subject = 'F5 CONFIGURATION CHANGES'
-        content = """
-<html>
-  <body>
-  <p>This is a test email for f5 config update.\n https://stash.mgmt.local/projects/MERC/repos/f5_backup/browse.</p>
-  <p>Changes in files.</p>
-    <pre style="color:green;">{}</pre>
-  </body>
-</html>
-""".format('\n'.join('<span style="color:blue;">{}</span>'.format(file) for file in tracked_files))
-        print("Tracked files:")
-        print(tracked_files)
-        send_email(smtp_server, sender_email, recipient_email, subject, content, body_as_html=True)
+        # Add and commit changes locally
+        if git_add_and_commit(repo, 'Automated commit - Changes detected'):
+            # Check for changes in the remote repository and pull if necessary
+            if git_pull(repo):
+                # Push changes to the remote repository
+                tracked_files = get_tracked_files_from_repo(target_directory)
+                print("Tracked files:")
+                print(tracked_files)
+                if git_push(repo):
+                        print("Process completed successfully.")
+                        # Example usage
+                        smtp_server = 'os-smtpp702.prod.mdgapp.net'
+                        sender_email = 'sonali.jain@ihsmarkit.com'
+                        recipient_email = ['sonali.jain@ihsmarkit.com']
+                        subject = 'F5 CONFIGURATION CHANGES'
+                        content = """
+                                    <html>
+                                      <body>
+                                      <p>This is a test email for f5 config update.\n https://stash.mgmt.local/projects/MERC/repos/f5_backup/browse.</p>
+                                      <p>Changes in files.</p>
+                                        <pre style="color:green;">{}</pre>
+                                      </body>
+                                    </html>
+                                    """.format('\n'.join('<span style="color:blue;">{}</span>'.format(file) for file in tracked_files))
+                        send_email(smtp_server, sender_email, recipient_email, subject, content, body_as_html=True)
+                else:
+                        print("Failed to push changes to remote repository.")
+            else:
+                print("Failed to pull changes from remote repository.")
+        else:
+            print("Failed to stage and commit changes locally.")
     else:
-        print("No changes detected.")
+        print("No local changes detected in file.")
 
 if __name__ == "__main__":
     main()
+
